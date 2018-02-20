2194ba6006210dd465109f734a636d78d748e0c4
Alexey Zapparov <ixti@member.fsf.org>
2018-02-20 16:02:53 +0100

    Improve wording in redis spec support

--- spec/support/redis.rb
+++ spec/support/redis.rb
@@ -2,25 +2,26 @@
 
 require "redis"
 
-REDIS_URL       = ENV.fetch("REDIS_URL", "redis://localhost/15").freeze
-REDIS_NAMESPACE = ENV["REDIS_NAMESPACE"].to_s.strip
-REDIS_RAW       = Redis.new(:url => REDIS_URL)
+REDIS_URL         = ENV.fetch("REDIS_URL", "redis://localhost/15").freeze
+REDIS_NAMESPACE   = ENV["REDIS_NAMESPACE"].to_s.strip
+REDIS_CONNECTION  = Redis.new(:url => REDIS_URL)
 
 REDIS =
   if REDIS_NAMESPACE.empty?
-    REDIS_RAW
+    REDIS_CONNECTION
   else
     require "redis-namespace"
-    Redis::Namespace.new(REDIS_NAMESPACE, :redis => REDIS_RAW)
+    Redis::Namespace.new(REDIS_NAMESPACE, :redis => REDIS_CONNECTION)
   end
 
 RSpec.configure do |config|
-  cleanup = proc { REDIS_RAW.tap(&:flushdb).script("flush") }
+  cleanup = proc { REDIS_CONNECTION.tap(&:flushdb).script("flush") }
 
   config.before :suite do
     puts "***"
     puts "*** REDIS_URL=#{REDIS_URL.inspect}"
     puts "*** REDIS_NAMESPACE=#{REDIS_NAMESPACE.inspect}"
+    puts "*** REDIS=#{REDIS.inspect}"
     puts "***"
   end
 
